f745100ea1febc85756c4f2f2761461b82523c9c
r3wrk <harelsh21@gmail.com>
2024-05-18 19:12:26 +0300

    Add files via upload

--- 2/q2.py
+++ 2/q2.py
@@ -3,7 +3,9 @@ from math import isqrt
 
 def pythagorean_triplet_by_sum(s):
     for c in range(1, s):
-        # a**2+(apb-a)**2=csq  =>
+        # a**2+(apb-a)**2=csq  => 2*a**2-2*apb*a+(apb**2-csq)=0
+        # => a=(-2±sqrt(4*apb**2-4*2*(apb**2-csq))/(2*2)
+        # => a=(-1±sqrt(2*csq - apb**2))/(2)
         apb = s - c
         csq = c ** 2
         d = 2 * csq - apb ** 2
@@ -17,9 +19,9 @@ def pythagorean_triplet_by_sum(s):
         print((apb - d_root) // 2, (apb + d_root) // 2, c)
 
 
-def pythagorean_triplet_by_sum_v0(s):  ##
+def pythagorean_triplet_by_sum_v0(s):
     for c in range(s):
-        for b in range(max(1, (s - c) // 2 + ((s - c) % 2)), min(c, s - c)):
+        for b in range(1, c):
             a = s - c - b
-            if a ** 2 + b ** 2 == c ** 2:
+            if 0 < a < b < c and a ** 2 + b ** 2 == c ** 2:
                 print(a, b, c)
